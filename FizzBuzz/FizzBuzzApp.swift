a96ba93d24e2c48667874c41d19cb674b6c6fbb9
Ralf Ebert <info@ralfebert.de>
2021-11-05 13:16:25 +0100

    empty project

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by Ralf Ebert on 05.11.21.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
